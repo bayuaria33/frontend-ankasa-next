a5c98035fdae3f34be161cd4e17ab0a1cc88c1b3
Ariabayu <Bayuaria33@gmail.com>
2023-05-02 14:51:49 +0700

    updated landing page

--- src/components/landing/trending.js
+++ src/components/landing/trending.js
@@ -1,10 +1,63 @@
+import Image from "next/image";
+import bar from "../../../public/barcelona.jpg";
+
 export const Trending = () => {
   return (
-    <div className="md:px-16 px-4 my-4">
+    <div className="md:px-16 px-4 my-6">
       <p className="md:block text-md hidden text-ankasa-blue">TRENDING</p>
       <p className="text-2xl text-black md:font-bold">Trending Destinations</p>
-      <div className="text-black flex my-4">
-        <div className="h-64 w-52 bg-gray-500 rounded-2xl"></div>
+      <div className="h-64 w-52 bg-gray-500 rounded-2xl mx-4 md:hidden flex"></div>
+      <div className="text-black my-4 hidden md:flex justify-center">
+        {/* 1 */}
+        <div
+          className="h-72 w-52 rounded-2xl mx-4 flex-col flex justify-end p-3 text-xl font-bold text-white"
+          style={{
+            backgroundImage: `url(${bar.src})`,
+          }}
+        >
+          <p>Barcelona,</p>
+          <p>Spain</p>
+        </div>
+        {/* 2 */}
+        <div
+          className="h-72 w-52 rounded-2xl mx-4 flex-col flex justify-end p-3 text-xl font-bold text-white"
+          style={{
+            backgroundImage: `url(${bar.src})`,
+          }}
+        >
+          <p>Barcelona,</p>
+          <p>Spain</p>
+        </div>
+        {/* 3 */}
+        <div
+          className="h-72 w-52 rounded-2xl mx-4 flex-col flex justify-end p-3 text-xl font-bold text-white"
+          style={{
+            backgroundImage: `url(${bar.src})`,
+          }}
+        >
+          <p>Barcelona,</p>
+          <p>Spain</p>
+        </div>
+        {/* 4 */}
+        <div
+          className="h-72 w-52 rounded-2xl mx-4 flex-col flex justify-end p-3 text-xl font-bold text-white"
+          style={{
+            backgroundImage: `url(${bar.src})`,
+          }}
+        >
+          <p>Barcelona,</p>
+          <p>Spain</p>
+        </div>
+        {/* 5 */}
+        <div
+          className="h-72 w-52 rounded-2xl mx-4 flex-col flex justify-end p-3 text-xl font-bold text-white"
+          style={{
+            backgroundImage: `url(${bar.src})`,
+          }}
+        >
+          <p>Barcelona,</p>
+          <p>Spain</p>
+        </div>
       </div>
     </div>
   );
@@ -28,14 +81,29 @@ export const Popular = () => {
 
 export const Explore = () => {
   return (
-    <div className="hidden md:block md:px-16 px-4 my-4">
-      <p className="text-4xl font-extrabold text-black ">
-        Find your <span className="text-ankasa-blue">Flight</span>
-      </p>
-      <p className="text-gray-600 my-4">And explore the world with us</p>
-      <div className="w-2/3 h-96 bg-gray-500 rounded-2xl">
-
+    <div className="hidden xl:block md:px-16 px-4 my-4">
+      <div className="w-11/12 bg-transparent flex justify-between absolute">
+        <div>
+          <p className="text-4xl font-extrabold text-black ">
+            Find your <span className="text-ankasa-blue">Flight</span>
+          </p>
+          <p className="text-gray-600 my-4">And explore the world with us</p>
+        </div>
+        <Image
+          src={"/japan.jpg"}
+          width={500}
+          height={250}
+          alt="japan"
+          className="rounded-2xl mt-16 hidden xl:block shadow-2xl"
+        />
       </div>
+      <Image
+        src={"/japan.jpg"}
+        width={800}
+        height={400}
+        alt="japan"
+        className="rounded-2xl -ml-24 mt-40 shadow-2xl"
+      />
     </div>
   );
 };
